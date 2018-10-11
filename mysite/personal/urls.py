46f8c5f7891cbd5b406889b7fb28744eab91eabf
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-10-11 16:16:04 +0200

    cleanup

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -18,5 +18,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('contact/', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact')
 ]
