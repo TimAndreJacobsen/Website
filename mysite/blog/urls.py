433dcccedb12a23f04caa9b85a535ee52d14eeec
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-10-18 16:08:30 +0200

    added path for each individual post
    path is an auto incrementing primary key.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,6 @@ from blog.models import Post
 from . import views
 
 urlpatterns = [ 
-    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:10], template_name="blog/blog.html"))
+    path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:10], template_name="blog/blog.html")),
+    path('<pk>', DetailView.as_view(model = Post, template_name="blog/post.html"))
     ]
