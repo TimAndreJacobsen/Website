1bffb0c99e028b40dfdb5ab5701a0ff712605c93
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-10-18 14:19:54 +0200

    added sample content to views.py
    added comments to contact()

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -5,4 +5,8 @@ def index(request):
     return render(request, "personal/home.html")
 
 def contact(request):
+    #basic.html for c in content. iterates through list passed in param
     return render(request, "personal/basic.html", {"content":["Email:", "@gmail.com"]})
+
+def include_sample(request):
+    return render(request, "personal/sample_content.html")
