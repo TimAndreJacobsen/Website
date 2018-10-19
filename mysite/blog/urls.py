03769371cc021ae080e219f305d8cd7f663d9b1c
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-10-19 13:24:10 +0200

    Update mysite/blog/urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
-from django.views.generic import ListView, DetailView
+from django.urls import include, path
+from django.views.generic import DetailView, ListView
+
 from blog.models import Post
 
 from . import views
 
 urlpatterns = [ 
     path('', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:10], template_name="blog/blog.html")),
-    path('<pk>', DetailView.as_view(model = Post, template_name="blog/post.html"))
+    path('<pk>', DetailView.as_view(model=Post, template_name="blog/post.html"))
     ]
